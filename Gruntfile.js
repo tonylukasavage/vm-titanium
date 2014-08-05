f1059153bdfc8a8f53159ce2877c706391de341f
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-08-04 22:45:21 -0400

    create test harness

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+var fs = require('fs'),
+	path = require('path');
+
+var TMP_DIR = 'tmp';
+
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		titanium: {
+			create: {
+				options: {
+					command: 'create',
+					name: TMP_DIR,
+					workspaceDir: '.',
+					platforms: ['android','ios']
+				}
+			},
+			build: {
+				options: {
+					command: 'build',
+					projectDir: TMP_DIR,
+					logLevel: 'info',
+					platform: 'ios',
+					iosVersion: '7.1'
+				}
+			}
+		},
+		clean: {
+			src: [TMP_DIR]
+		}
+	});
+
+	// Load grunt plugins for modules
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-titanium');
+
+	// load test app
+	grunt.registerTask('app-prep', 'Load source files into example alloy app', function() {
+
+		var resourcesDir = path.join(TMP_DIR, 'Resources');
+
+		// copy in app.js
+		fs.writeFileSync(path.join(resourcesDir, 'app.js'),
+			fs.readFileSync(path.resolve('test', 'app.js')));
+
+		// copy in vm-titanium.js
+		fs.writeFileSync(path.join(resourcesDir, 'vm-titanium.js'),
+			fs.readFileSync(path.resolve('vm-titanium.js')));
+
+	});
+
+	// run example app
+	grunt.registerTask('test', ['titanium:create', 'app-prep', 'titanium:build']);
+
+	// Register tasks
+	grunt.registerTask('default', 'test');
+
+};
