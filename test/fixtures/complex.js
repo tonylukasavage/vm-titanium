9bb447ad96078897d7beb7b4b3861303aed1c88d
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-08-15 08:44:25 -0400

    stop the color warnings

--- test/fixtures/complex.js
+++ test/fixtures/complex.js
@@ -7,12 +7,15 @@ var button = Ti.UI.createButton({
 win.add(button);
 
 for (var i = 0; i < 10; i++) {
+	var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+	while (color.length < 7) { color = color.substr(0,1) + '0' + color.substr(1); }
+
 	win.add(Ti.UI.createView({
 		height: Math.random() * Ti.Platform.displayCaps.platformHeight,
 		width: Math.random() * Ti.Platform.displayCaps.platformWidth,
 		left: Math.random() * Ti.Platform.displayCaps.platformWidth,
 		top: Math.random() * Ti.Platform.displayCaps.platformHeight,
-		backgroundColor: '#'+Math.floor(Math.random()*16777215).toString(16)
+		backgroundColor: color
 	}));
 }
 
