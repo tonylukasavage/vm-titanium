ce730615225d0ab24a41f9d0a97db1ea91722c54
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-08-31 12:07:57 -0400

    major Gruntfile.js simplification with grunt-titanium 0.2.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,22 +7,11 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-		titanium: {
-			create: {
-				options: {
-					command: 'create',
-					name: TMP_DIR,
-					workspaceDir: '.',
-					platforms: ['android','ios']
-				}
-			},
-			build: {
-				options: {
-					command: 'build',
-					projectDir: TMP_DIR,
-					logLevel: 'info',
-					platform: 'ios',
-					iosVersion: '7.1'
+		ti_run: {
+			all: {
+				files: {
+					'tmp/all/Resources': ['test/*', 'vm-titanium.js', '__context.js',
+						'node_modules/should/should.js', 'node_modules/ti-mocha/ti-mocha.js']
 				}
 			}
 		},
@@ -35,32 +24,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-titanium');
 
-	// load test app
-	grunt.registerTask('app-prep', 'Load source files into example alloy app', function() {
-
-		var resourcesDir = path.join(TMP_DIR, 'Resources'),
-			to = function(file) {
-				return path.join(resourcesDir, file);
-			};
-
-		// copy in app.js
-		fs.copy(path.resolve('test', 'app.js'), to('app.js'));
-		fs.copy(path.resolve('test', 'fixtures'), to('fixtures'));
-
-		// copy in vm-titanium.js and context.js
-		fs.copy(path.resolve('vm-titanium.js'), to('vm-titanium.js'));
-		fs.copy(path.resolve('__context.js'), to('__context.js'));
-
-		// copy in ti-mocha and should
-		fs.copy(path.resolve('node_modules', 'ti-mocha', 'ti-mocha.js'), to('ti-mocha.js'));
-		fs.copy(path.resolve('node_modules', 'should', 'should.js'), to('should.js'));
-
-	});
-
-	// run example app
-	grunt.registerTask('test', ['titanium:create', 'app-prep', 'titanium:build']);
-
 	// Register tasks
-	grunt.registerTask('default', 'test');
+	grunt.registerTask('default', ['clean', 'ti_run']);
 
 };
